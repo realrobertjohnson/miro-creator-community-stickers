80b3637ef286a3d08f24c0722f24f66aaa3a1dbe
realrobertjohnson <127041638+realrobertjohnson@users.noreply.github.com>
2024-01-31 16:34:20 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import type { DropEvent } from "@mirohq/websdk-types";
+
+const { board } = miro;
+
+function App() {
+  const images = [
+		"kangaroo.svg",
+    "koala.svg",
+  ];
+
+  const drop = async (e: DropEvent) => {
+    const { x, y, target } = e;
+
+    if (target instanceof HTMLImageElement) {
+      const image = await board.createImage({ x, y, url: target.src });
+      await board.viewport.zoomTo(image);
+    }
+  };
+
+  // Register the drop event handler once.
+  useEffect(() => {
+    board.ui.on("drop", drop);
+  });
+
+  return (
+    <div className="main">
+      {images.map((image, index) => {
+        return (
+          <img
+            src={image}
+            draggable={false}
+            className="miro-draggable draggable-item"
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+// Render App
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
