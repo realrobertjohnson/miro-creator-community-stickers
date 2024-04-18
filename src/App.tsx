d3df18e34542a6a47adfd561cfe88039012cf5dd
realrobertjohnson <127041638+realrobertjohnson@users.noreply.github.com>
2024-04-18 14:53:09 -0600

    Vedran's changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,97 @@ import ReactDOM from "react-dom";
 
 import type { DropEvent } from "@mirohq/websdk-types";
 
-/*
-Example of event listener for button click: https://github.com/miroapp/app-examples/tree/main/examples/template-builder (in src/app.js)
-*/
-
 const { board } = miro;
 
+const drop = async (e: DropEvent) => {
+  const { x, y, target } = e;
+
+  if (target instanceof HTMLImageElement) {
+    const image = await board.createImage({ x, y, url: target.src });
+    await board.viewport.zoomTo(image);
+  }
+};
+
 function App() {
-
-  const imagesStickersA = [
-		"stickers-a/letter-a-1.svg",
-		"stickers-a/letter-a-2.svg",
-		"stickers-a/letter-a-3.svg",
-		"stickers-a/letter-a-4.svg",
+  const stickerPacks = [
+    {
+      title: "Stickers A",
+      images: [
+        "stickers-a/letter-a-1.svg",
+        "stickers-a/letter-a-2.svg",
+        "stickers-a/letter-a-3.svg",
+        "stickers-a/letter-a-4.svg",
+      ],
+    },
+    {
+      title: "Stickers B",
+      images: [
+        "stickers-b/letter-b-1.svg",
+        "stickers-b/letter-b-2.svg",
+        "stickers-b/letter-b-3.svg",
+        "stickers-b/letter-b-4.svg",
+      ],
+    },
+    {
+      title: "Stickers C",
+      images: [
+        "stickers-a/letter-a-1.svg",
+        "stickers-a/letter-a-2.svg",
+        "stickers-a/letter-a-3.svg",
+        "stickers-a/letter-a-4.svg",
+      ],
+    },
+    {
+      title: "Stickers D",
+      images: [
+        "stickers-b/letter-b-1.svg",
+        "stickers-b/letter-b-2.svg",
+        "stickers-b/letter-b-3.svg",
+        "stickers-b/letter-b-4.svg",
+      ],
+    },
   ];
 
-  // add new arrays – one for each sticker pack
-  const imagesStickersB = [
-		"stickers-b/letter-b-1.svg",
-		"stickers-b/letter-b-2.svg",
-		"stickers-b/letter-b-3.svg",
-		"stickers-b/letter-b-4.svg",
-  ];
-
-  const drop = async (e: DropEvent) => {
-    const { x, y, target } = e;
-
-    if (target instanceof HTMLImageElement) {
-      const image = await board.createImage({ x, y, url: target.src });
-      await board.viewport.zoomTo(image);
-    }
-  };
+  const [curStickerPackIndex, setCurStickerPackIndex] = React.useState(0);
 
   // Register the drop event handler once.
   useEffect(() => {
     board.ui.on("drop", drop);
   });
 
+  const curStickerPack = stickerPacks[curStickerPackIndex];
+
   return (
     <div className="main">
-      {imagesStickersA.map((image, index) => {
-        return (
-          <img
-            src={image}
-            draggable={false}
-            className="miro-draggable draggable-item"
-            key={index}
-          />
-        );
-      })}
+      <div>Drag & drop a sticker onto the board:</div>
+      <br />
+      <div className="scroll">
+        {stickerPacks.map((stickerPack, index) => {
+          return (
+            <button
+              id="addRow"
+              className="add-row-button"
+              onClick={() => setCurStickerPackIndex(index)}
+              key={index}
+            >
+              {stickerPack.title}
+            </button>
+          );
+        })}
+      </div>
+
+      <div className="sticker-images">
+        {curStickerPack.images.map((image, index) => {
+          return (
+            <img
+              src={image}
+              draggable={false}
+              className="miro-draggable draggable-item"
+              key={index}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
